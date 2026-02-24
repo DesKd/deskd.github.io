6f19818fd3751cfd66be89a6270a936319073a8b
Carlos Borges <113952127+DesKd@users.noreply.github.com>
2026-02-24 15:54:37 -0300

    Refactor function to use names from JSON import

--- scripite.js
+++ scripite.js
@@ -1,19 +1,21 @@
-function escolherNomeAleatorio() {
+import nomes1 from './names.json' with {type: 'json'};
+
+window.chamarNome = function () {
     let divres = document.getElementById('res');
     let nome = document.createElement('p');
 
-    if (!Array.isArray(nomes) || nomes.length === 0) {
+    if (!Array.isArray(nomes1) || nomes1.length === 0) {
         throw new Error("O array 'nomes' não está definido ou está vazio.");
     }
 
-    const indiceAleatorio = Math.floor(Math.random() * nomes.length);
+    const indiceAleatorio = Math.round(Math.random() * nomes1.length);
     //let nomeAleatorio = nomes[indiceAleatorio];
 
     while(divres.firstChild) {
         divres.removeChild(divres.firstChild)
     } 
 
-    nome.innerText = nomes[indiceAleatorio];
+    nome.innerText = nomes1[indiceAleatorio];
     divres.appendChild(nome);
     
 }
